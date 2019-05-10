1ee523fb71f06b2ed8c426a3cba12cd9ea3e9266
Mayank2905 <mayankbhogal29@gmail.com>
2019-05-11 02:52:44 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,4 @@
 node default {
- file {'/root/README':
-  ensure => file,
-  content => 'This is a puppet tutorial developed by Mayank Bhogal',
-  }
 }
 node 'master.puppet.vm'{
  include role::master_server
