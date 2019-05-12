47b2729ccabb414f8349deebec272bdc85678f74
Mayank2905 <mayankbhogal29@gmail.com>
2019-05-13 01:54:05 +0530

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDomIcMXGcVsLecKTYMzNs70kKQ0nqvL4l/ZkeGJIhS6bBkW13/gTwmPc1yw8QE3XZ3pLa2oMKdecgmjNwWH6G8ClxIlaoy6W/mBjrQYUjmtgfC7t8k0tkzg29SVTK/7ywkXUlGbClp7lubHWFiEvKVF03p3LNsepvSwypyzEub2R8YpW6HfBSAY64F2jKPsKzk8Cac8fRJiR7aTzD0CdPM4Uxelw0XLoexlYvI8pHQ2Ns/YkMB5zfWgzeCl0RQ9f5cXovIPLLOSh+XYeoEJhk9yI6DPdgW02HjGOOdStWLt+KSVuKb1jLJT7viDGHYUSo3U8BBV0qCnB/vmaLgjGX7',
   }  
 }
